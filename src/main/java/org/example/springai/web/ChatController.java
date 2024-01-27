fa73bdde90adb4e52f24d7d4620aef49122d45f5
Yeonwoo <kyu@whatap.io>
2024-01-27 13:35:34 +0900

    Implement content-only streaming in ChatController
    The updated 'generateStreamContentOnly' method in ChatController now uses a filtering process, facilitated by the 'isFinished' helper function. This implementation replaces the previously commented code block and ensures more efficient processing of response streams. To align with these changes, the API test call has been updated correspondingly.

--- src/main/java/org/example/springai/web/ChatController.java
+++ src/main/java/org/example/springai/web/ChatController.java
@@ -30,9 +30,19 @@ public class ChatController {
         return Map.of("generation", chatClient.call(message));
     }
 
-//    @GetMapping("/open-ai/generateStream")
-//    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-//        Prompt prompt = new Prompt(new UserMessage(message));
-//        return streamingChatClient.(prompt);
-//    }
+    @GetMapping("/open-ai/generate/content-only")
+    public Flux<String> generateStreamContentOnly(@RequestParam(defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(message);
+        return streamingChatClient.stream(prompt)
+                .filter(chatResponse -> !isFinished(chatResponse))
+                .map(chatResponse -> chatResponse.getResult().getOutput().getContent());
+    }
+
+    /**
+     * https://github.com/Hyune-s-lab/practice-spring-ai/commit/378eeab7b21e0044ac502484ee508a94e54a0d1c
+     */
+    private boolean isFinished(ChatResponse chatResponse) {
+        return chatResponse.getResult().getOutput().getContent() == null
+                || "STOP".equals(chatResponse.getResult().getMetadata().getFinishReason());
+    }
 }
